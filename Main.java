3eb9b12dc28cb10faaccf2ecf57096db768b5053
Nishanthi S <144414534+Nishanthi666@users.noreply.github.com>
2025-02-25 19:40:24 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,36 @@
+//Practice 1
+// public class Main
+// {
+// 	public static void main(String[] args) {
+// 	    boolean cricket = false;
+// 	    boolean football = false;
+// 	    if(cricket || football){
+// 	        System.out.println("Play");
+// 	    }
+// 	    else{
+// 	        System.out.println("Don't Play");
+	        
+// 	    }
+// 	}
+// }
+
+//Practice 2
+// Check whether the number is divisible by 3 and 5 or not.If yes then print,
+//the number is divisible by 3 and 5, print the number not divisible by 3 and 5.
+
+// import java.util.Scanner;
+// public class Main{
+//     public static void main(String args[]){
+//         Scanner scan = new Scanner(System.in);
+//         int num1 = scan.nextInt();
+//         if(num1%3==0 && num1%5==0){
+//             System.out.println("The number is divisible by 3 and 5");
+//         }
+//         else{
+//             System.out.println("The Number is not divisible");
+//         }
+//         }
+// } 
+
+//Practice 3
+//
